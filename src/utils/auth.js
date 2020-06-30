21d4f141dd81979d17caa5f0fab7fabbfd776af0
Pramod Vadrevu <pramodvadrevu@gmail.com>
2020-06-30 00:36:14 -0500

    WIP

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,10 +1,14 @@
 import { Auth0Lock, Auth0LockPasswordless } from "auth0-lock"
 
 const isBrowser = typeof window !== "undefined"
+let params = {}
 const options = {
   passwordlessMethod: "link",              // Sets Lock to use magic link
   auth: {
     responseType: 'token id_token',
+    params: {
+      access_type: 'offline',
+    },    
   },
   languageDictionary: {
       emailInputPlaceholder: "something@youremail.com",
@@ -16,22 +20,57 @@ const options = {
   theme:{
       primaryColor: '#002fa7'
   },
+  allowedConnections: params.domain ? domainArr : ['google-oauth2','email']
 }
-let lock = isBrowser ? new Auth0LockPasswordless( `kcmqY88kyS2IYFquoscBgIIy4LRUh6me`, `dev-nseui4sk.us.auth0.com`, options ) : {}
+let lock = isBrowser ? new Auth0LockPasswordless( `NEfeuai07t5BIyCI4F2d3iTHOK4LJPW1`, `staging-dynosio.auth0.com`, options ) : {}
 if(isBrowser) {
   lock.on('authenticated', function(authResult) {
+    createSessionCookie(authResult)
     redirect()
   });
   lock.on('authorization_error', function(err){
-      lock = new Auth0Lock(`kcmqY88kyS2IYFquoscBgIIy4LRUh6me`, `dev-nseui4sk.us.auth0.com`, options)
+      lock = new Auth0Lock(`NEfeuai07t5BIyCI4F2d3iTHOK4LJPW1`, `staging-dynosio.auth0.com`, options)
       lock.show();
   })
 }
+function createSessionCookie(authResult) {
+  console.log("Creating session cookie.")
+  var v = "dyn_session="+authResult.idToken+";domain="+getDomain(window)+";expires="+getExpirationDate().toGMTString();
+  console.log(v)
+  document.cookie = v;      
+}
+
 function redirect() {
   if(typeof document !== `undefined`){
       document.location.href = "http://localhost:8000";
   }
 }
+
+async function setGoogleAccessTokenToCookie(idToken) {
+  const response = await fetch(LOGIN_ENDPOINT, {
+    method: 'POST',
+    body: JSON.stringify({
+      query: `
+          query { userIdentity }
+        `,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': idToken,
+    },
+  })
+
+  const { data: { userIdentity: { identities = [] } } } = await response.json()
+  const googleIdentity = identities.find(({ provider }) => provider === 'google-oauth2')
+
+  if (googleIdentity) {
+    const expirationDate = new Date()
+    expirationDate.setTime(expirationDate.getTime() + 365 * 24 * 60 * 60 * 1000)
+    if(typeof window !== `undefined` && typeof document !== `undefined`)
+        document.cookie = `accessToken=${googleIdentity.access_token};domain=${getDomain(window)};expires="${expirationDate}`;
+  }
+}
+
 export const login = () => {
   if (!isBrowser) {
     return 
